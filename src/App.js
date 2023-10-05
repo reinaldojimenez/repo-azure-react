20391779a2323aa45626d6551492e8a6b4119829
rjimenez <reinaldo.jimenez.montero@gmail.com>
2023-10-05 07:46:53 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Proyecto de Azure y React.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          React desplegado en Azure con GitHub.
+        </p>        
       </header>
     </div>
   );
